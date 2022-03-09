fe700ade18f83ad60798510a151a07e9010fa191
hp <hp@LAPTOP-H5JN7IVF>
2022-03-09 19:47:33 +0530

    Switch Statement

---
+++ C2TC-CORE-JAVA/src/com/corejava/Firstjava.java
@@ -0,0 +1,25 @@
+package com.corejava;
+
+public class Firstjava {
+
+	public static void main(String[] args) {
+		int x=10;
+		switch(x) {
+		case 0:
+			System.out.println(0);
+			break;
+			
+		case 1:
+			System.out.println(1);
+		case 2:
+			System.out.println(2);
+		default:
+			System.out.println("Default statement");
+		}
+		
+		
+		
+		
+	}
+
+}
